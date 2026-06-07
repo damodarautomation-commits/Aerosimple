56a169a03363186a1164589edb0506bb9cc902d5
Damodar Ippili <damodarautomationcommits@gmail.com>
2026-06-07 10:33:21 +0530

    Aerosimple test passed successfully

--- playwright.config.js
+++ playwright.config.js
@@ -1,24 +1,20 @@
 import { defineConfig } from '@playwright/test';
 import 'dotenv/config';
-
 export default defineConfig({
   testDir: './tests',
   timeout: 30 * 1000,
   retries: 0,
   workers: 1,
   reporter: 'html',
-
   use: {
-    headless: true,
+    headless: false,
     trace: 'on',
     viewport: null,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-
     launchOptions: {
       args: ['--start-maximized'],
     },
-
     baseURL: process.env.BASE_URL,
   },
 });
