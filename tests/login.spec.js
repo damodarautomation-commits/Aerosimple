f67dd5afdf9b4d2e996d9a291ec7926aafb48bf4
Damodar Ippili <damodarautomationcommits@gmail.com>
2026-06-07 11:19:07 +0530

    Aerosimple test passed successfully

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,5 +4,6 @@ import LoginPage from '../pages/loginpage.js';
 test('Aerosimple Login', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await page.goto('/');
-    await loginPage.login(process.env.USERNAME,process.env.PASSWORD);
+    console.log('Navigated to login page');
+    await loginPage.login(process.env.EMAIL,process.env.PASSWORD);
 });
